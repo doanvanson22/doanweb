1a2ec6c2e5782e49a131eaf4f2703bf1244e6839
doanvanson22 <sondd202@gmail.com>
2023-03-20 13:53:36 +0700

    son

---
+++ doancuoimon/Controllers/SanPhamController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doancuoimon.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public ActionResult about()
+        {
+            return View();
+        }
+
+        public ActionResult service()
+        {
+            return View();
+        }
+        
+        public ActionResult carlisting()
+        {
+            return View();
+        }
+
+        public ActionResult cardetali()
+        {
+            return View();
+        }
+
+        public ActionResult carbooking()
+        {
+            return View();
+        }
+
+        public ActionResult theteam()
+        {
+            return View();
+        }
+
+        public ActionResult testimonail()
+        {
+            return View();
+        }
+
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ doancuoimon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(doancuoimon.Startup))]
+namespace doancuoimon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
